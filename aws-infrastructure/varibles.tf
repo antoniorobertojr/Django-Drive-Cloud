db7e4fe93216571c6ad6f67ee5c63a8a916f0d36
Antonio Roberto <juniormach96@gmail.com>
2024-02-06 12:08:03 -0500

    format terraform files

--- aws-infrastructure/varibles.tf
+++ aws-infrastructure/varibles.tf
@@ -1,9 +1,9 @@
 variable "region" {
-    description = "Aws region to create resources in"
-    default = "us-east-2"
+  description = "Aws region to create resources in"
+  default     = "us-east-2"
 }
 
 variable "project_name" {
-    description = "Project name to use in resources names"
-    default = "django-file-manager"
+  description = "Project name to use in resources names"
+  default     = "django-file-manager"
 }
