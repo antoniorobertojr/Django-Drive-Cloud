ee7ec084fd7319df8e18f796f5fdb91d0a72b892
Antonio Roberto <juniormach96@gmail.com>
2024-02-07 10:02:16 -0500

    add rds database with postgres engine

--- aws-infrastructure/variables.tf
+++ aws-infrastructure/variables.tf
@@ -13,8 +13,27 @@ variable "availability_zones" {
   default     = ["us-east-2a", "us-east-2c"]
 }
 
+# ecs
+
 variable "ecs_prod_backend_retention_days" {
   description = "Retention period for backend logs"
   default     = 30
 }
 
+# rds
+
+variable "prod_rds_db_name" {
+  description = "RDS database name"
+  default     = "django_aws"
+}
+variable "prod_rds_username" {
+  description = "RDS database username"
+  default     = "django_aws"
+}
+variable "prod_rds_password" {
+  description = "postgres password for production DB"
+}
+variable "prod_rds_instance_class" {
+  description = "RDS instance type"
+  default     = "db.t4g.micro"
+}
