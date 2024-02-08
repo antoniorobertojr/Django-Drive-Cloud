ccd8d5045e02802b488ed1ca2981734ff93002bf
Antonio Roberto <juniormach96@gmail.com>
2024-02-08 08:48:17 -0500

    add s3 bucket for terraform backend

--- aws-infrastructure/provider.tf
+++ aws-infrastructure/provider.tf
@@ -1,3 +1,10 @@
 provider "aws" {
   region = var.region
 }
+terraform {
+  backend "s3" {
+    bucket = "terraform-django-file-manager-backend"
+    key    = "terraform"
+    region = "us-east-2"
+  }
+}
