cfd67f98056c7304fb51f4b96f06c6afcccb1774
Antonio Roberto <juniormach96@gmail.com>
2024-02-16 10:34:28 -0500

    add ecr_repository to terraform outputs

--- aws-infrastructure/outputs.tf
+++ aws-infrastructure/outputs.tf
@@ -2,3 +2,7 @@ output "prod_lb_domain" {
   value = aws_lb.prod.dns_name
 }
 
+output "ecr_repository" {
+  value = aws_ecr_repository.backend.repository_url
+  description = "The repository URL for the backend ECR repository."
+}
